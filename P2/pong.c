049b2f972f9858b6f27aba16710611c32fe69e2f
jcruzlp <jcruzlp97@gmail.com>
2022-04-08 11:42:52 +0200

    Añadido pong completo

--- P2/pong.c
+++ P2/pong.c
@@ -1,9 +1,11 @@
-// gcc pong.c -o pong -lncurses
+// gcc pong.puntos -o pong -lncurses
 
 #include <stdlib.h>
 #include <ncurses.h>
 
-#define DELAY 30000
+#define DELAY 6000
+typedef struct{short int x, y, puntos; bool horizontal, vertical;} object;
+int i = 0;
 
 void instrucciones(){
   int rows, cols;
@@ -40,7 +42,10 @@ void instrucciones(){
   mvwprintw(window, 18, 10, "Las instrucciones son simples, estas son:");
   mvwprintw(window, 19, 10, "Jugador 1, tus controles son las teclas W para moverta a arriba y S para abajo.");
   mvwprintw(window, 20, 10, "Jugador 2, tus controles son las flechas hacia arriba y hacia abajo para tener esos movimientos respectivamente.");
-  mvwprintw(window, 21, 10, "Pulsa cualquier tecla para empezar!");
+  mvwprintw(window, 21, 10, "Pulsa P para pausa.");
+  mvwprintw(window, 21, 10, "Pulsa 1, 2 o 3 durante la partida para escoger la dificultad (de menor a mayor).");
+  mvwprintw(window, 22, 10, "Pulsa ESC para salir.");
+  mvwprintw(window, 23, 10, "Pulsa cualquier tecla para empezar!");
   wrefresh(window);
 
   getch(); 
@@ -51,32 +56,80 @@ void instrucciones(){
 int main(void) {
   instrucciones();
 
-  int x = 0, y = 0;
-  int max_y = 20, max_x = 20;
-  int next_x = 0;
-  int direction = 1;
-
+  object pantalla; 
+  int cont = 0;
+  int dificultad = 1;
+  bool fin = 0;
   initscr();
+  start_color();
+  init_pair(1,COLOR_BLUE,COLOR_BLACK);
+  keypad(stdscr,true);
   noecho();
-  curs_set(FALSE);
+  curs_set(0);
+  getmaxyx(stdscr,pantalla.y,pantalla.x);
+  object jugador1={pantalla.x-2,pantalla.y/2,0,false,false};
+  object jugador2={1,pantalla.y/2,0,false,false};
+  object pelotita={pantalla.x/2,pantalla.y/2,0,false,false};
 
-  while(1) {
-  clear();
-  mvprintw(y, x, "o");
-  refresh();
+  //Movimientos de pelotita y barras
+  for (nodelay(stdscr,1); !fin; usleep(DELAY)){
+    if (++cont%16==0){
+      if ((pelotita.y==pantalla.y-1)||(pelotita.y==1))
+        pelotita.vertical=!pelotita.vertical;
+      if ((pelotita.x>=pantalla.x-2)||(pelotita.x<=2)){
+        pelotita.horizontal=!pelotita.horizontal;
+        if ((pelotita.y==jugador1.y-1)||(pelotita.y==jugador2.y-1)) {
+          pelotita.vertical=false;
+        } else if ((pelotita.y==jugador1.y+1)||(pelotita.y==jugador2.y+1)) {
+          pelotita.vertical=true;
+        } else if ((pelotita.y != jugador1.y) && (pelotita.y != jugador2.y)) {
+          (pelotita.x>=pantalla.x-2) ? jugador1.puntos++: jugador2.puntos++;
+          pelotita.x=pantalla.x/2;
+          pelotita.y=pantalla.y/2;
+        }
+      }
 
-  usleep(DELAY);
+      pelotita.x=pelotita.horizontal ? pelotita.x+1 : pelotita.x-1;
+      pelotita.y=pelotita.vertical ? pelotita.y+1 : pelotita.y-1;
 
-  next_x = x + direction;
+      if (jugador1.y<=1)
+        jugador1.y=pantalla.y-2;
+      if (jugador1.y>=pantalla.y-1)
+        jugador1.y=2;
+      if (jugador2.y<=1)
+        jugador2.y=pantalla.y-2;
+      if (jugador2.y>=pantalla.y-1)
+        jugador2.y=2;
+    }
 
-  if (next_x >= max_x || next_x < 0) {
-    direction*= -1;
-  } else {
-    x+= direction;
+    //Capturar de teclado
+    switch (getch()) {
+      case KEY_DOWN: jugador1.y++; break;
+      case KEY_UP:   jugador1.y--; break;
+      case 'w':      jugador2.y--; break;
+      case 's':      jugador2.y++; break;
+      case '1':      dificultad = 3; break;
+      case '2':      dificultad = 2; break;
+      case '3':      dificultad = 1; break;
+      case 'p':      getchar(); break;
+      case 0x1B:     endwin(); fin++; break;
+    }
+
+    erase();
+
+    //Pintar puntutaciones
+    mvprintw(2,pantalla.x/2-2,"%i | %i",jugador1.puntos,jugador2.puntos);
+    mvvline(0,pantalla.x/2,ACS_VLINE,pantalla.y);
+    attron(COLOR_PAIR(1));
+
+    //Pintar la pelotita
+    mvprintw(pelotita.y,pelotita.x,"o");
+
+    //Pintar las barras
+    for(i=-1;i<2*dificultad;i++){
+      mvprintw(jugador1.y+i,jugador1.x,"#");
+      mvprintw(jugador2.y+i,jugador2.x,"#");}
+    attroff(COLOR_PAIR(1));
   }
-  }
-
-  endwin();
-
 
 }
